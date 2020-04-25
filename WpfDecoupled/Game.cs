aab07b5c5c562d5029707a54e1c06da83f0abe51
Tony Collins <tjc@eltham-college.org.uk>
2020-04-25 10:56:05 +0100

    Add project files.

---
+++ WpfDecoupled/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDecoupled
+{
+    class Game
+    {
+        private int x = 0;
+        public void buttonClick()
+        {
+            x++;
+            
+        }
+        public int getX()
+        {
+
+            return x;
+        }
+    }
+}
